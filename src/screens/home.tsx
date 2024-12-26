2d8abb80e4a81b649178507563f9cd255e5c404b
Kenny Ho <kennyho.code@gmail.com>
2024-12-26 10:14:35 -0500

    fix deps

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -7,25 +7,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     backgroundColor: "#ecf0f1",
-    padding: 8
+    padding: 8,
   },
   title: {
     margin: 24,
     fontSize: 18,
     fontWeight: "bold",
-    textAlign: "center"
+    textAlign: "center",
   },
   subTitle: {
     margin: 24,
-    textAlign: "center"
-  }
+    textAlign: "center",
+  },
 });
 
 const Home = () => {
   return (
     <View style={styles.container}>
       <Logo />
-      <Text style={styles.title}>MightyByte React Native Challenge.</Text>
+      <Text style={styles.title}>MightyByte React Native Challenge. hello</Text>
       <Text style={styles.subTitle}>
         You are allowed to modify this project structure in any way you wish.
       </Text>
